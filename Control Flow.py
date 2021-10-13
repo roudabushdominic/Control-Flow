94aa1d61e8c5cddc408760bc31a4c684cf0e3239
Dominic <roudabushcs@gmail.com>
2021-10-13 09:49:16 -0700

    Add First and Last Name Variables
    We declared our first and last name statement by concatenating the two variables

--- Control Flow.py
+++ Control Flow.py
@@ -1,3 +1,15 @@
 # Developer : Dominic Roudabush
 # Date: 10.11.2021
-# Program: Control Flow 
+# Program: ATM Bank Transaction
+
+"""
+This Program simulates an ATM utilizing If, ELif, and Else statements
+Nesting out If statements and refresh our Comparison & Logical Operators
+"""
+
+print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n")
+
+# Set up account by asking users for their first and last name using variables
+
+firstName = input("What is your first name: ") 
+lastName = input(" What is your last name: ")
