e3c94f0326adf28f84b07d1dd088e0b3e3e9f740
Dominic <roudabushcs@gmail.com>
2021-10-14 09:54:04 -0700

    Created a PIN variable

--- Control Flow.py
+++ Control Flow.py
@@ -12,4 +12,13 @@ print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n"
 # Set up account by asking users for their first and last name using variables
 
 firstName = input("What is your first name: ") 
-lastName = input(" What is your last name: ")
+lastName = input("What is your last name: ")
+
+print("Welcome to Cash-R-Us", firstName, lastName + ", we will now set up a security PIN on your account.\n") 
+
+# Set up a PIN - Personal Identification Number
+
+pin = input("Please choose a 4-digit security pin: ")
+
+print("\nThank you", firstName + ", We see that you set your PIN to", pin)
+
