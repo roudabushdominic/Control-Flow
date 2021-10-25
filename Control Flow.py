51b5fd3171e31069399675ae60f4a104cc27d226
Dominic <roudabushcs@gmail.com>
2021-10-25 09:49:02 -0700

    Add ATM Variable and If,Else Statements

--- Control Flow.py
+++ Control Flow.py
@@ -10,15 +10,24 @@ Nesting out If statements and refresh our Comparison & Logical Operators
 print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n")
 
 # Set up account by asking users for their first and last name using variables
-
 firstName = input("What is your first name: ") 
 lastName = input("What is your last name: ")
 
-print("Welcome to Cash-R-Us", firstName, lastName + ", we will now set up a security PIN on your account.\n") 
+print("Welcome to Cash-R-Us", firstName, lastName + ", we will now set up a security PIN on your account.\n")
 
-# Set up a PIN - Personal Identification Number
+# Set up a pin -Personal Identification Number
+pin = input("Please choose a 4 digit Personal Identification Number:")
 
-pin = input("Please choose a 4-digit security pin: ")
+print("\nThank you", firstName + ",we see that you set you pin to", pin)
 
-print("\nThank you", firstName + ", We see that you set your PIN to", pin)
+print("\nWould you like to make a transaction through our Automated Teller Machine")
+atm = input("Yes or No: ").lower()
+
+if atm == "yes":
+    print("\n*********************************************************\n")
+
+
+else:
+    print("\nHave a wonderful day",firstName,lastName + ", please come back and visit soon.")
+
 
